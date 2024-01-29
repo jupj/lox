97747f9f4f1ded7aeb225cb246801c4467c86cad
Johan Jansson <johan.jansson@iki.fi>
2024-01-29 20:09:23 +0200

    Add expression and print statements (section 8.1)

--- src/main/java/com/github/jupj/jlox/Parser.java
+++ src/main/java/com/github/jupj/jlox/Parser.java
@@ -1,5 +1,6 @@
 package com.github.jupj.jlox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.github.jupj.jlox.TokenType.*;
@@ -14,16 +15,36 @@ class Parser {
         this.tokens = tokens;
     }
 
-    Expr parse() {
-        try {
-            return expression();
-        } catch (ParseError error) {
-            return null;
+    List<Stmt> parse() {
+        List<Stmt> statements = new ArrayList<>();
+        while (!isAtEnd()) {
+            statements.add(statement());
         }
+
+        return statements;
     }
 
     private Expr expression() {
         return equality();
+    }
+
+    private Stmt statement() {
+        if (match(PRINT))
+            return printStatement();
+
+        return expressionStatement();
+    }
+
+    private Stmt printStatement() {
+        Expr value = expression();
+        consume(SEMICOLON, "Expect ',' after value.");
+        return new Stmt.Print(value);
+    }
+
+    private Stmt expressionStatement() {
+        Expr value = expression();
+        consume(SEMICOLON, "Expect ',' after value.");
+        return new Stmt.Expression(value);
     }
 
     private Expr equality() {
